56a03d1c9171405648d6b777452e379dcb5e2ca9
Mehmet Can ÜNALDI <mehmetcanunaldi@gmail.com>
2024-03-15 21:02:33 +0300

    first commit

---
+++ OnlineMuhasebeServer/OnlineMuhasebeServer.Presentation/Controller/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineMuhasebeServer.Presentation.Abstraction;
+
+namespace OnlineMuhasebeServer.Presentation.Controller;
+public sealed class TestController : ApiController
+{
+    [HttpGet]
+    public IActionResult Test()
+    {
+        return Ok("Api çalışıyor");
+    }
+}
